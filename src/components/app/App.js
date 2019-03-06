5995611afdc26257e40244c8fcc2924f57e1509b
Damon <damonasberg@gmail.com>
2019-03-06 13:57:39 +0100

    Added home contact info, favicons and styling

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react'
-import './App.css'
+import './App.scss'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+      <div className="app">
+        <div className="intro">
+          <h1 className="name">Damon</h1>
+          <h1 className="lastname">Asberg</h1>
+        </div>
+        <div className="socials">
+          <a href="mailto:damon@damon.dev" target="_self" rel="noopener noreferrer">
+            <div className="socialbutton mail"/>
           </a>
-        </header>
+          <a href="https://www.github.com/damon02" target="_blank" rel="noopener noreferrer">
+            <div className="socialbutton github"/>
+          </a>
+          <a href="https://www.linkedin.com/in/damon02/" target="_blank" rel="noopener noreferrer">
+            <div className="socialbutton linkedin"/>
+          </a> 
+        </div>
+        <h2 className="mail">damon@damon.dev</h2>
+        <div className="copy">&copy; 2019 - damon.dev</div>
       </div>
     )
   }
